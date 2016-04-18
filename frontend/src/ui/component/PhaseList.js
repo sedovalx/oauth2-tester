942afa57551edf8eaa3bb9b4454da9b33fc94df3
sedovalx <sedov.alx@gmail.com>
2016-04-19 01:09:49 +0300

    Phases availability

--- frontend/src/ui/component/PhaseList.js
+++ frontend/src/ui/component/PhaseList.js
@@ -9,21 +9,28 @@ const PhaseList = React.createClass({
         const phases = this.props.phases;
         const availablePhases = this.props.availablePhases;
         const flows = this.props.flows;
+        const isPhaseAvailable = name => availablePhases.indexOf(name) >= 0;
         return (
             <div className="phase-list">
                 <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
                     <ServerEditorModalContainer />
-                    <PhaseItem key={phases.selectServer.name} phase={phases.selectServer} isActive={true}>
-                        <div className="phase-item phase-select-server">
-                            <ServerListContainer />
-                        </div>
-                    </PhaseItem>
-                    <PhaseItem key={phases.queryToken.name} phase={phases.queryToken}>
-                        <div className="phase-item phase-query-token"></div>
-                    </PhaseItem>
-                    <PhaseItem key={phases.queryData.name} phase={phases.queryData}>
-                        <div className="phase-item phase-query-data"></div>
-                    </PhaseItem>
+                    {isPhaseAvailable(phases.selectServer.name) && (
+                        <PhaseItem key={phases.selectServer.name} phase={phases.selectServer} isActive={true}>
+                            <div className="phase-item phase-select-server">
+                                <ServerListContainer />
+                            </div>
+                        </PhaseItem>
+                    )}
+                    {isPhaseAvailable(phases.queryToken.name) && (
+                        <PhaseItem key={phases.queryToken.name} phase={phases.queryToken}>
+                            <div className="phase-item phase-query-token"></div>
+                        </PhaseItem>
+                    )}
+                    {isPhaseAvailable(phases.queryData.name) && (
+                        <PhaseItem key={phases.queryData.name} phase={phases.queryData}>
+                            <div className="phase-item phase-query-data"></div>
+                        </PhaseItem>
+                    )}
                 </div>
             </div>
         );
