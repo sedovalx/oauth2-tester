0a8d1b6a8da6d5b696f04f1af70f810f840b0ce7
alsedov <zaq1xsw2>
2016-04-20 22:09:01 +0300

    Get back hidden phases

--- frontend/src/reducers/commons.js
+++ frontend/src/reducers/commons.js
@@ -1,10 +1,15 @@
+const flows = [
+    { name: "CODE_FLOW", desc: "Code flow" },
+    { name: "RESOURCE_FLOW", desc: "Resource Owner Flow" },
+    { name: "CLIENT_FLOW", desc: "Client Flow" }
+];
+
 const commonState = {
     title: "OAuth 2.0 Tester",
-    flows: [
-        { name: "CODE_FLOW", desc: "Code flow" },
-        { name: "RESOURCE_FLOW", desc: "Resource Owner Flow" },
-        { name: "CLIENT_FLOW", desc: "Client Flow" }
-    ]
+    flows: {
+        items: flows,
+        current: flows[0]
+    }
 };
 
 export default function(state = commonState) {
