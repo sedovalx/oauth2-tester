61d6108b4abd18d6ef880ac1524f6e5668f9c6ed
sedovalx <sedov.alx@gmail.com>
2016-04-24 23:00:14 +0300

    #10: fix initial state parsing

--- frontend/src/reducers/current.js
+++ frontend/src/reducers/current.js
@@ -26,16 +26,19 @@ export default function(state = defaultState, action) {
             if (action.payload.flow) {
                 newState = u({flow: action.payload.flow}, newState);
             }
-            return newState;  
+            if (action.payload.auth) {
+                newState = u({auth: action.payload.auth}, newState);
+            }
+            return newState;
         case actionTypes.FETCH_SERVERS_END:
             if (state.server && typeof state.server !== 'object' && !action.error) {
                 // try find server by its restored from url name
                 const name = state.server;
                 const loadedServers = action.payload.items;
                 return u({
-                    server: loadedServers.filter(s => s.name === name)[0]
+                    server: () => loadedServers.filter(s => s.name === name)[0]
                 }, state);
-            }
+            }  
             return state;
         case actionTypes.SERVER_SELECTED:
             return u({server: action.payload}, state);
